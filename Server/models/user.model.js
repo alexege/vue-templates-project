d0e1f7e71f35d01b7e18f9701d1c77d6bda91c34
Alexander Ege <alexander@ege.com>
2024-05-15 00:12:09 -0700

    Updated server.js file to add connection string to mongodb database

---
+++ Server/models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+    "User",
+    new mongoose.Schema({
+        authId: String,
+        username: String,
+        email: String,
+        password: String,
+        roles: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Role"
+            }
+        ],
+        img: String,
+        comments: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Comment"
+            }
+        ],
+        todos: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Todo"
+            }
+        ],
+    },
+    { timestamps: true})
+);
+
+module.exports = User;
--- Server/server.js
+++ Server/server.js
@@ -20,19 +20,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = require("./models");
 const dbConfig = require("./config/db.config");
 
+//Model Connection
 // const User = require("./models/user.model")
 
-
+db.mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("Successfully connected to Mongodb!");
+  //Triggering the database seeding if no items exist
+  // init();
+})
+.catch((err) => {
+  console.log("Connection error", err);
+  process.exit();
+});
 
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Alexander's Vue Templates Project." });
 });
 
+// routes
+require('./routes/user.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+//Seed the database
+function init() {
+  //TODO
+}
 
 // https://www.bezkoder.com/node-express-mongodb-crud-rest-api/
